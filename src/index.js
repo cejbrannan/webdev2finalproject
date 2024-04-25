dc3e6c719c791dae5584e03dcf567bb70cc87957
Derek Abeln <111763839+dabeln52@users.noreply.github.com>
2024-04-24 19:37:41 -0500

    Changed it to use mongoose and added basic model

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
-var Express = require('express');
-var cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+var express = require('express');
+var cors = require('cors');
+var mongoose = require('mongoose');
 
-var app = Express();
+var app = express();
 app.use(cors());
 
+const uri = "mongodb+srv://4830:Password@cluster0.uu5wuim.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const uri = "mongodb+srv://4830:Password@cluster0.uu5wuim.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+  });
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected successfully to MongoDB using Mongoose!");
 });
 
-async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  review: String,
+  recommend: String
+
+});
+
+const User = mongoose.model('User', userSchema);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
